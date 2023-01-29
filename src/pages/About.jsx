ba035d8cd8bf368ce71f859086223caf3fe99052
Sophia Montes Bolah <sophiamb@Sophias-MacBook-Pro.local>
2023-01-29 15:28:59 +0000

    Add page routing

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+
+function About() {
+    return (
+      <>
+        <h1 className='text-6xl mb-4'>Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+          A React app to search GitHub profiles and see profile details. 
+        </p>
+        <p className='text-lg text-gray-400'>
+          Version 1.0.0
+        </p>
+
+      </>
+    )
+  }
+
+export default About
